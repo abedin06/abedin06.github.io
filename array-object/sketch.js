363e96fc0850d6d58761e5782cd66cdaf274f66d
abedin06 <7008752@spsd.sk.ca>
2024-10-20 14:12:34 -0600

    Added gamestates, start and end screens, fixed the problems with automated functions

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -26,10 +26,15 @@ let theta; //launch angle
 
 const G = -9.81;
 
-let value_entered = false;
-
 let time = 0;
 let terrainGraphics;
+
+let langle; //program generated launch angle
+let lspeed;// program generated launch speed
+
+let screenState = "homescreen";
+let struck_target = false;
+let decision = "nothing";
 
 
 
@@ -40,6 +45,9 @@ function setup() {
 
   playerX = Math.round(random(50,width/2-50));
   playerY = Math.round(random(200,height-200));
+
+  x = targetX+targetsize/2 - playerX;
+  y = playerY - (targetY+targetsize/2);
 
   angleMode(DEGREES);
 
@@ -68,16 +76,8 @@ function setup() {
 }
 
 function draw() {
-
-  image(terrainGraphics,0,0);
-  displayTarget();
-  displayPlayer();
-  displayPrompt();
-  displayGameInfo();
-  start_moving();
-  if (keyIsDown(76)){
-    give_up();
-  }
+  GameState();
+  ChangeState();
 }
 
 
@@ -92,11 +92,17 @@ function displayPlayer(){
   circle(playerX,playerY,playerSize*2);
 }
 
-function displayPrompt(){
+function player_Decision(){
   if (keyIsDown(70)){
-    v0 = prompt("Please enter your launch speed");
-    theta = prompt("Please enter your launch angle");
-    value_entered = true;
+    let prompt1 = prompt("Please enter your launch speed");
+    let prompt2 = prompt("Please enter your launch angle");
+    v0 = parseFloat(prompt1);
+    theta = parseFloat(prompt2);
+    decision = "play";
+  }
+
+  if(keyIsDown(76)){
+    decision = "give up";
   }
 }
 
@@ -142,25 +148,110 @@ function spawnRectangle(leftside, rectHeight, rectWidth){
   return theRect;
 }
 
-// Game functions
+
+
+// Ball movement functions
 
 function moveball(launch_angle,launch_speed){
 
-  time += 1/60;
-  playerX += cos(launch_angle)*launch_speed;
-  playerY -= v0*sin(launch_angle) + G*time;
+  time += 1/6;
+  playerX += cos(launch_angle)*launch_speed/6;
+  playerY -= (launch_speed*sin(launch_angle) + G*time)/6;
 }
 
 function start_moving(){
-  if(value_entered){
+  if(decision === "play"){
     moveball(theta,v0);
+  }
+
+  else if(decision === "give up"){
+    give_up();
   }
 }
 
 function give_up(){
-  let langle = 45;
-  let x = targetX+targetsize/2 - playerX;
-  let y = playerY - (targetY+targetsize/2);
-  let lspeed = 1/cos(theta) * (0.5*G*x*x/(x*tan(theta)-y))**1/2;
+  langle = 60;
+  lspeed = 1/cos(langle) * Math.sqrt(0.5*G*x*x/(y-x*tan(langle)));
   moveball(langle,lspeed);
+}
+
+// Gamestate functions
+
+function GameState(){
+
+  if (screenState === "homescreen"){
+    background(0);
+    textSize(50);
+    fill("white");
+    text("So you think you know Physics?", 400, 250);
+
+    let color1 = "white";
+    let color2 = "red";
+
+    if(mouseX > 600 && mouseX < 850 && mouseY > 350 && mouseY < 450){
+      color1 = "red";
+      color2 = "white";
+    }
+
+    fill(color1);
+    rect(600,350,250,100);
+
+    fill(color2);
+    textSize(70);
+    text("PLAY",635,420);
+  }
+
+  if(screenState === "gamescreen"){
+    image(terrainGraphics,0,0);
+    displayTarget();
+    displayPlayer();
+    player_Decision();
+    displayGameInfo();
+    start_moving();
+    hit_target();
+  }
+
+  if (screenState === "winscreen"){
+    background(0);
+    fill("white");
+    textSize(100);
+    text("You Won!",600,300);
+  }
+
+  if(screenState === "losescreen"){
+    background(0);
+    fill("white");
+    textAlign(CENTER,TOP);
+    textSize(100);
+    text("You Lost!",700,300);
+    textSize(70);
+    textAlign(CENTER,BASELINE);
+    text("It's back to Physical Science 20 for you.",750,500);
+  }
+
+}
+
+function ChangeState(){
+  if (screenState === "homescreen" && mouseIsPressed && (mouseX > 600 && mouseX < 850 && mouseY > 350 && mouseY < 450)){
+    screenState = "gamescreen";
+  }
+
+  if (screenState === "gamescreen" && struck_target && (playerX > windowWidth || playerY > windowHeight) && decision === "play"){
+    screenState = "winscreen";
+  }
+
+  if(screenState === "gamescreen" && !struck_target && (playerX > windowWidth || playerY > windowHeight)){
+    screenState = "losescreen";
+  }
+
+  if(screenState === "gamescreen" && struck_target && (playerX > windowWidth || playerY > windowHeight) && decision === "give up"){
+    screenState = "losescreen";
+  }
+}
+
+function hit_target(){
+  if (playerX+playerSize >= targetX && playerX+playerSize <= targetX+targetsize 
+    && playerY+playerSize >= targetY && playerY+playerSize <= targetY+targetsize){
+    struck_target = true;
+  }
 }
