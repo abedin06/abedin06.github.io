e53b6cac0bd6bd29a54d2faf75614cc85ed4f0f1
Abedin7008752 <7008752@spsd.sk.ca>
2024-11-01 13:52:28 -0600

    Made the player move

--- grid-project/sketch.js
+++ grid-project/sketch.js
@@ -6,26 +6,35 @@
 // - describe what you did to take this project "above and beyond"
 
 
-const CELL_SIZE = 20;
 let grid;
-let rows;
-let columns;
-let playerColor = "black";
+const GRID_SIZE = 40;
+let cell_size;
+
+let toggle = false;
 
 const PLAYER_TILE = 10;
 let player = {
   x: 10,
-  y: 10,
+  y: 20,
 };
 
 
+
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  rows = Math.floor(height/CELL_SIZE);
-  columns = Math.floor(width/CELL_SIZE);
-  grid = generateGrid(rows,columns);
+
+  if(windowWidth < windowHeight){
+    createCanvas(windowWidth,windowWidth);
+  }
+
+  else{
+    createCanvas(windowHeight, windowHeight);
+  }
+  cell_size = height/GRID_SIZE;
+  grid = generateRandomGrid(GRID_SIZE,GRID_SIZE);
+
+  // add player to the grid
   grid[player.y][player.x] = PLAYER_TILE;
-  
 }
 
 function draw() {
@@ -33,56 +42,130 @@ function draw() {
   displaygrid();
 }
 
-function generateGrid(ROWS,COLS){
+function keyPressed(){
+  if(key === "r"){
+    grid = generateRandomGrid(GRID_SIZE,GRID_SIZE);
+  }
+
+  if(key === "e"){
+    grid = generateEmptyGrid(GRID_SIZE,GRID_SIZE);
+  }
+
+  if(key === "n"){
+    toggle = !toggle;
+  }
+
+  if(keyCode === UP_ARROW){
+    //move up
+    movePlayer(player.x,player.y-1);
+  }
+
+  if(keyCode === DOWN_ARROW){
+    //move down
+    movePlayer(player.x,player.y+1);
+  }
+
+  if(keyCode === RIGHT_ARROW){
+    //move right
+    movePlayer(player.x+1,player.y);
+  }
+
+  if(keyCode === LEFT_ARROW){
+    //move left
+    movePlayer(player.x-1,player.y);
+  }
+}
+
+function movePlayer(x,y){
+
+  //Do not run of the screen
+  if (x>=0 && x < GRID_SIZE && y>=0 && y < GRID_SIZE){
+    //keep track of player location
+  
+    player.x = x;
+    player.y = y;
+  
+    //put player in grid
+    grid[player.y][player.x] = PLAYER_TILE;
+  }
+}
+
+function displaygrid(){
+  for (let y = 0; y < GRID_SIZE; y++){
+    for (let x = 0; x < GRID_SIZE; x++){
+      if (grid[y][x] === 0){
+        fill("white");
+      }
+
+      else if(grid[y][x] === PLAYER_TILE){
+        fill("blue");
+      }
+
+      square(x*cell_size,y*cell_size,cell_size);
+    }
+  }
+}
+
+function generateRandomGrid(columns,rows){
   let newGrid = [];
-  for(let y = 0; y < ROWS; y++){
+  for (let y = 0; y < rows; y++){
     newGrid.push([]);
-    for(let x = 0; x < COLS; x++){
+    for (let x = 0; x < columns; x ++){
+      newGrid[y].push(0);
+    }
+  }
+
+  return newGrid;
+}
+
+function generateEmptyGrid(columns,rows){
+  let newGrid = [];
+  for (let y = 0; y < rows; y++){
+    newGrid.push([]);
+    for (let x = 0; x < columns; x ++){
       newGrid[y].push(0);
     }
   }
   return newGrid;
 }
 
-function displaygrid(){
-  for(let y = 0; y < rows; y++){
-    for(let x = 0; x < columns; x++){
-      if(grid[y][x] === 0){
-        fill("white");
-        square(x*CELL_SIZE,y*CELL_SIZE,CELL_SIZE);
-      }
+function mousePressed(){
+  let x = Math.floor(mouseX/cell_size);
+  let y = Math.floor(mouseY/cell_size);
 
-      else if(grid[y][x] === PLAYER_TILE){
-        fill(playerColor);
-        square(x*CELL_SIZE,y*CELL_SIZE,CELL_SIZE);
-      }
+
+  //Toggle self
+  changecolor(x,y);
+
+  //Toggle neighboursr
+  if(toggle){
+    changecolor(x+1,y);
+    changecolor(x,y+1);
+    changecolor(x,y-1);
+    changecolor(x-1,y);
+  }
+}
+
+function windowResized(){
+  if(windowWidth < windowHeight){
+    resizeCanvas(windowWidth,windowWidth);
+  }
+
+  else{
+    resizeCanvas(windowHeight, windowHeight);
+  }
+  cell_size = height/GRID_SIZE;
+}
+
+function changecolor(some_x,some_y){
+  //BS checker
+  if (some_x >=0 && some_y >=0 && some_x < GRID_SIZE && some_y < GRID_SIZE){
+    if(grid[some_y][some_x] === 0){
+      grid[some_y][some_x] = PLAYER_TILE;
+    }
+
+    else{
+      grid[some_y][some_x] = 0;
     }
   }
 }
-
-function keyPressed(){
-  if(key === "r"){
-    playerColor = "red";
-  }
-
-  if(key === "b"){
-    playerColor = "blue";
-  }
-
-  if(key === "g"){
-    playerColor = "green";
-  }
-
-  if(key === "w"){
-    playerColor = "white";
-  }
-
-  if(key === "o"){
-    playerColor = "black";
-  }
-}
-
-function movePlayer(some_x,some_y){
-  
-}
-
